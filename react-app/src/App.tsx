69c23d9814b1e9fc7c1ac2f452a9d4d83c8a2601
Ekim <ekimbasoglu@hotmail.com>
2025-07-01 14:14:14 +0200

    Update App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -8,7 +8,7 @@ import {
 } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Register from "./pages/Register";
 
 const App: React.FC = () => {
   const token = localStorage.getItem("token");
